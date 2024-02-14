492bdc2d386c5f93331bc177840fc1c6aa960880
PustanAlex <alecsandru_bogdy@yahoo.com>
2024-02-14 21:10:42 +0200

    added 2nd modal

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const yesButton = document.getElementById('Yes');
 const noButton = document.getElementById('No');
+const approvalModalWindow = document.querySelector('.approval');
 const modalWindow = document.querySelector('.modal');
-const closeModalButton = document.getElementById('close-modal')
+const closeModalButton = document.getElementById('close-modal');
+const acceptedYesButton = document.getElementById('approve-button');
+const acceptedNoButton = document.getElementById('not-approved');
 
 
 yesButton.addEventListener('mouseover', () => {
@@ -14,18 +17,29 @@ noButton.addEventListener('mouseover', () => {
     yesButton.textContent = "No";
 });
 
+
+
 yesButton.addEventListener('click', () => {
     if (yesButton.textContent === 'Yes') {
-        modalWindow.classList.add('accepted');
+        approvalModalWindow.classList.add('approve');
     }
 })
 
 noButton.addEventListener('click', () => {
     if (noButton.textContent === 'Yes') {
-        modalWindow.classList.add('accepted');
+        approvalModalWindow.classList.add('approve');
     }
 })
 
 closeModalButton.addEventListener('click', () => {
     modalWindow.classList.remove('accepted');
+})
+
+acceptedYesButton.addEventListener('click', () => {
+        modalWindow.classList.add('accepted');
+        approvalModalWindow.classList.remove('approve');
+   })
+
+acceptedNoButton.addEventListener('click', () => {
+    approvalModalWindow.classList.remove('approve')
 })
